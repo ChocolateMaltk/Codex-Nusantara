09eb9a6e0773d15df82a7918ecced1d573b3282d
malik <malik.bazil@student.umn.ac.id>
2023-05-16 23:59:43 +0700

    Some Homepage Improvements

--- ffid-app/pages/index.js
+++ ffid-app/pages/index.js
@@ -5,7 +5,7 @@ import "../styles/globals.css";
 const Home = () => {
   return (
     <div>
-      <header className="bg-gray-800 py-4">
+      <header className="bg-darkFlora py-4">
         <nav className="container mx-auto px-4 flex items-center justify-between">
           <a href="#" className="text-white font-bold text-xl">
             Logo
@@ -33,12 +33,12 @@ const Home = () => {
       <main className="container mx-auto px-4 py-8">
         <h1 className="text-4xl font-bold mb-4">Welcome To Flora Fauna Indonesia!</h1>
         <p className="text-lg text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacinia feugiat lacus, id auctor magna egestas vel.</p>
-        <a href="#" className="bg-blue-500 text-white px-4 py-2 mt-4 inline-block rounded hover:bg-blue-600">
+        <a href="#" className="bg-sky-300	 text-white px-4 py-2 mt-4 inline-block rounded hover:bg-blue-600">
           Get Started
         </a>
       </main>
 
-      <footer className="bg-gray-800 py-4 text-center text-white">
+      <footer className="bg-darkFlora py-4 text-center text-white">
         <p>&copy; 2023 Kelompok 7 Kelas C. All rights reserved.</p>
       </footer>
     </div>
--- ffid-app/pages/layout.js
+++ ffid-app/pages/layout.js
@@ -1,10 +1,4 @@
 import Head from "next/head";
-import About from "../../pages/about";
-import Flora from "../../pages/Content Pages/Flora";
-import Fauna from "../../pages/ Content Pages/Fauna";
-import Daerah from "../../pages/Content Pages/Daerah";
-import Home from "../../pages/page";
-import LandingPage from "../../pages/LandingPage";
 
 export default function Layout({ children }) {
   return (
