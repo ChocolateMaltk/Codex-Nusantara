7c6814f42bb90007dd16393b29ddd414896aed5f
malik <malik.bazil@student.umn.ac.id>
2023-05-16 23:03:33 +0700

    MAJOR UPDATE 1: HOME PAGE YANG BERFUNGSI

--- ffid-app/pages/index.js
+++ ffid-app/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
 
-const LandingPage = () => {
+const Home = () => {
   return (
     <div>
       <header className="bg-gray-800 py-4">
@@ -29,7 +31,7 @@ const LandingPage = () => {
       </header>
 
       <main className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-4">Welcome to our Simple Landing Page</h1>
+        <h1 className="text-4xl font-bold mb-4">Welcome To Flora Fauna Indonesia!</h1>
         <p className="text-lg text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacinia feugiat lacus, id auctor magna egestas vel.</p>
         <a href="#" className="bg-blue-500 text-white px-4 py-2 mt-4 inline-block rounded hover:bg-blue-600">
           Get Started
@@ -37,10 +39,10 @@ const LandingPage = () => {
       </main>
 
       <footer className="bg-gray-800 py-4 text-center text-white">
-        <p>&copy; 2023 Simple Landing Page. All rights reserved.</p>
+        <p>&copy; 2023 Kelompok 7 Kelas C. All rights reserved.</p>
       </footer>
     </div>
   );
 };
 
-export default LandingPage;
+export default Home;
