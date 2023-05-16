01a000c33460fa32fa3e1095ac8dea7d9d8f2026
achmadayubi99 <120900421+achmadayubi99@users.noreply.github.com>
2023-05-16 19:40:01 +0700

    Update layout.js

--- ffid-app/src/app/layout.js
+++ ffid-app/src/app/layout.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import About from "../../pages/about";
+import Flora from "../../pages/Flora";
+import Fauna from "../../pages/Fauna";
+import Daerah from "../../pages/Daerah";
 import Home from "./page";
 
 export default function Layout({ children }) {
