aa01f60068cebe93fd05e53da4741e26b5c7c3c4
malik <malik.bazil@student.umn.ac.id>
2023-05-22 22:42:22 +0700

    Update home page.

--- ffid-app/app/page.tsx
+++ ffid-app/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <h1 className="mt-12 mb-12 text-4xl text-center dark:text-white">
         Flora Fauna Indonesia
       </h1>
-      <h2 className="mt-12 mb-12 text-2xl text-center dark:text-white">Indonesia adalah negara kepulauan yang terdapat beraneka ragam makhluk hidup.
-      <br />Terletak diantara 2 benua yaitu, Asia dan Australia, <br/> menyebabkan Indonesia memiliki flora dan fauna yang khas dan berbeda dari wilayah lain.</h2>
+      <h2 className="mt-12 mb-12 text-2xl text-center dark:text-white">Indonesia merupakan negara kepulauan yang memiliki beraneka ragam makhluk hidup.
+      <br />Letak geografis Indonesia yang berada di antara benua Asia dan Australia, <br/> menyebabkan negara ini memiliki flora dan fauna yang khas.</h2>
       <Posts />
     </main>
   )
