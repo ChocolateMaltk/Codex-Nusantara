9e45fa060f99ed9ce9c7c61ca5a03027c6e2c90b
malik <malik.bazil@student.umn.ac.id>
2023-06-13 19:10:41 +0700

    Tab Persebaran untuk filter Flora dan Fauna mengetahui mereka dari daerah mana

--- ffid-app/app/perseberan/australis/page.tsx
+++ ffid-app/app/perseberan/australis/page.tsx
@@ -1,9 +1,43 @@
 "use client";
+import React from "react";
+import data from "./data.json";
 
 export default function Australis() {
+  const sortedData = data.sort((a, b) => a.name.localeCompare(b.name));
+
+  const redirect = (url: string) => {
+    window.location.href = url;
+  };
+
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className="mt-6 mb-6 text-center text-4xl text-black">
+      <h1 className="text-3xl font-bold mb-6 text-black">
+        Flora dan Fauna Australis
+      </h1>
+      <div className="flex flex-wrap justify-center gap-8">
+        {sortedData.map((item) => (
+          <div
+            key={item.name}
+            className="max-w-xs bg-yellow-200/70 rounded-lg p-4 shadow-lg"
+          >
+            <h1 className="text-lg font-bold mb-2">{item.name}</h1>
+            <img
+              src={item.imageUrl}
+              alt={item.name}
+              className="w-512 h-512 rounded-full mx-auto mb-4"
+            />
+            <p className="text-black text-sm h-24 overflow-hidden">
+              {item.flavorText}
+            </p>
+            <button
+              className="bg-violet-500 text-white text-sm py-2 px-4 rounded-full focus:outline-none hover:bg-violet-600"
+              onClick={() => redirect(item.btnURL)}
+            >
+              Baca Lebih Lanjut
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- ffid-app/app/perseberan/page.tsx
+++ ffid-app/app/perseberan/page.tsx
@@ -1,62 +1,67 @@
-"use client"
 import React from "react";
+import Link from "next/link";
 
-export default function persebaran() {
-    return (
-      <div className="mt-6 mb-6 flex flex-col items-center">
-        <div className="text-black text-2xl mt-4 mb-4 font-bold text-center">
-          <h2>Persebaran Flora dan Fauna di Indonesia</h2>
-        </div>
-        <div className="flex justify-center">
-          <p className="m-4 p-4 inline-block rounded-lg text-black text-lg bg-white text-center">
-            Seperti yang sekilas sudah dijelaskan pada homepage,
-            <br />
-            <b>Flora dan Fauna Indonesia</b> dibagi sesuai dengan persebarannya
-            menjadi tiga wilayah yaitu
+export default function Persebaran() {
+  return (
+    <div className="mt-6 mb-6 flex flex-col items-center">
+      <div className="text-black text-2xl mt-4 mb-4 font-bold text-center">
+        <h2>Persebaran Flora dan Fauna di Indonesia</h2>
+      </div>
+      <div className="flex justify-center">
+        <p className="m-4 p-4 inline-block rounded-lg text-black text-lg bg-white text-center">
+          Seperti yang sekilas sudah dijelaskan pada homepage,
+          <br />
+          <b>Flora dan Fauna Indonesia</b> dibagi sesuai dengan persebarannya
+          menjadi tiga wilayah yaitu
+        </p>
+      </div>
+      <div className="grid m-2 p-2 grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
+          <h1 className="text-xl font-bold text-black mb-2 text-center">
+            Asiatis
+          </h1>
+          <p className="h-32 w-64 text-gray-600 mb-4 text-center">
+            Flora dan Fauna Asiatis adalah mereka yang memiliki kemiripan atau
+            saudara di Benua Asia. Flora dan Fauna ini bisa ditemukan di Pulau
+            Sumatera, Jawa dan Kalimantan.
           </p>
+          <div className="flex justify-center mt-2">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <Link href={"/perseberan/asiatis"}>Read More</Link>
+            </button>
+          </div>
         </div>
-        <div className="grid m-2 p-2 grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-          <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
-            <h1 className="text-xl font-bold text-black mb-2 text-center">
-              Asiatis
-            </h1>
-            <p className="text-gray-600 mb-4 text-center">
-              Flora dan Fauna Asiatis adalah mereka yang memiliki kemiripan atau saudara di Benua Asia. Flora dan Fauna ini bisa ditemukan di Pulau Sumatera, Jawa dan Kalimantan.
-            </p>
-            <div className="flex justify-center">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Button 1
-              </button>
-            </div>
+        <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
+          <h1 className="text-xl font-bold text-black mb-2 text-center">
+            Indonesia Tengah
+          </h1>
+          <p className="h-32 w-64 text-gray-600 mb-4 text-center">
+            Flora dan Fauna Indonesia Tengah adalah Flora dan Fauna yang khusus
+            ditemukan di Indonesia Bagian Tengah seperti di Pulau Sulawesi,
+            Kepulauan Nusa Tenggara dan sekitarnya.
+          </p>
+          <div className="flex justify-center mt-2">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <Link href={"/perseberan/indonesia-tengah"}>Read More</Link>
+            </button>
           </div>
-          <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
-            <h1 className="text-xl font-bold text-black mb-2 text-center">
-              Indonesia Tengah
-            </h1>
-            <p className="text-gray-600 mb-4 text-center">
-              Flora dan Fauna Indonesia Tengah adalah Flora dan Fauna yang khusus ditemukan di Indonesia Bagian Tengah seperti di Pulau Sulawesi, Kepulauan Nusa Tenggara dan sekitarnya.
-            </p>
-            <div className="flex justify-center">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Button 2
-              </button>
-            </div>
-          </div>
-          <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
-            <h1 className="text-xl font-bold text-black mb-2 text-center">
-              Australis
-            </h1>
-            <p className="text-gray-600 mb-4 text-center">
-              Flora dan Fauna Australis adalah Flora dan Faunya yang khusus ditemukan di Indonesia Bagian Timur seperti di Kepulauan Maluku, Pulau Papua dan sekitarnya.
-            </p>
-            <div className="flex justify-center">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Button 3
-              </button>
-            </div>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
+          <h1 className="text-xl font-bold text-black mb-2 text-center">
+            Australis
+          </h1>
+          <p className="h-32 w-64 text-gray-600 mb-4 text-center">
+            Flora dan Fauna Australis adalah Flora dan Faunya yang khusus
+            ditemukan di Indonesia Bagian Timur seperti di Kepulauan Maluku,
+            Pulau Papua dan sekitarnya.
+          </p>
+          <div className="flex justify-center mt-2">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <Link href={"/perseberan/australis"}>Read More</Link>
+            </button>
           </div>
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
