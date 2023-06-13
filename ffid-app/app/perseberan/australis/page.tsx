a6e4b7a4621095bde83621835a9926274349c74f
malik <malik.bazil@student.umn.ac.id>
2023-06-13 16:17:02 +0700

    Boilerplate biar vercel jalan

---
+++ ffid-app/app/perseberan/australis/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Australis() {
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  );
+}
---
+++ ffid-app/app/perseberan/indonesia-tengah/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function IndonesiaTengah() {
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  );
+}
